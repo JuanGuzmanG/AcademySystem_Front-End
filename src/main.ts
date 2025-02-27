9028ba83db4ed48c550ff661650b6e367353b49e
juanguzman <juanjguzman15@gmail.com>
2025-02-27 12:46:18 -0500

    component init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { MATERIAL_IMPORTS } from './app/material.imports';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  providers: [
+    appConfig.providers,
+    MATERIAL_IMPORTS
+  ]
+})
   .catch((err) => console.error(err));
