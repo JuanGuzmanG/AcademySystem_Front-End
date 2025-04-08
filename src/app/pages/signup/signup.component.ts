5bf023f63e4a56f7d57a7027c65f5855b8ec4cd8
juanguzman <juanjguzman15@gmail.com>
2025-04-08 15:23:55 -0500

    form

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,106 +1,92 @@
-import { Component } from '@angular/core';
-import { materialModule } from '../../material.imports';
+import { Component, OnInit } from '@angular/core';
+import { materialImports } from '../../material.imports';
 import Swal from 'sweetalert2';
 import { UserService } from '../../services/user.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { FormControl, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'app-signup',
-  imports: [materialModule],
+  imports: [materialImports()],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
 })
-export class SignupComponent{
-  
-  public customgender='';
-  Controldocument  = new FormControl('',[Validators.required])
-  ControllastName = new FormControl('',[Validators.required])
-  ControlPassword = new FormControl('',[Validators.required,Validators.minLength(8)])
-  DTControl = new FormControl('', [Validators.required]);
-  BTControl = new FormControl('');
-  EmailControl = new FormControl('',[Validators.required, Validators.email]);
-  types = ['Cedula', 'Identity Card', 'Passport', 'PPT'];
-  BT = ['-','A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
-
-  public user = {
-    documentType: '',
-    document: '',
-    firstName: '',
-    middleName: '',
-    lastName: '',
-    secondLastName:'',
-    birthDate: '',
-    email: '',
-    password: '',
-    countryBirth:'',
-    phoneNumber:'',
-    gender:'',
-    bloodType:'',
-    photo:'',
-  }
-  
-  constructor(private userService:UserService, private snack:MatSnackBar,
-    private http: HttpClient){}
-  
-  ngOnInit() {
-    this.getCountries();
-  }
-  
+export class SignupComponent {
   countries: string[] = [];
+  types = ['Cedula', 'Identity Card', 'Passport', 'PPT'];
+  BT = ['-', 'A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
+  customGender = '';
+
+  signupForm: FormGroup;
+
+  constructor(
+    private userService: UserService,
+    private snack: MatSnackBar,
+    private http: HttpClient,
+    private fb: FormBuilder
+  ) {
+    this.getCountries();
+    this.signupForm = this.fb.group({
+      documentType: ['', Validators.required],
+      document: ['', Validators.required],
+      firstName: ['', Validators.required],
+      middleName: [''],
+      lastName: ['', Validators.required],
+      secondLastName: [''],
+      birthDate: [''],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      countryBirth: [''],
+      phoneNumber: [''],
+      gender: [''],
+      bloodType: [''],
+      photo: [''],
+      customGender: ['']
+    });
+  }
+
   getCountries() {
-    this.http.get<any[]>('https://restcountries.com/v3.1/all').subscribe(data => {
-      this.countries = data.map(country => country.name.common).sort();
-    },
-    (error)=>{
-      console.error('error library')
-    }
-  );
-  }
-
-  formSubmit(){
-    if(this.user.gender===''){
-      this.user.gender=this.customgender;
-    }
-    
-    if(this.user.document==='' || this.DTControl.invalid || this.user.firstName===''
-      || this.user.lastName==='' || this.EmailControl.invalid || this.ControlPassword.invalid){
-        
-        this.snack.open('fill out the required fields','OK',{
-          duration: 3000,
-          verticalPosition: 'top'
-        })
-        return;
-      }
-    
-    this.userService.addUser(this.user).subscribe(
+    this.http.get<any[]>('https://restcountries.com/v3.1/all').subscribe(
       (data) => {
-        Swal.fire('user saved successfully','user saved in the system','success')
-      },(error) => {
-        this.snack.open('System Error, Try later','OK',{
-          duration: 3000,
-          verticalPosition: 'top'
-        })
+        this.countries = data.map((c) => c.name.common).sort();
+      },
+      (error) => {
+        console.error('Error fetching countries');
       }
-    ); 
+    );
   }
 
-  clear(){
-    this.user = {
-      documentType: '',
-    document: '',
-    firstName: '',
-    middleName: '',
-    lastName: '',
-    secondLastName:'',
-    birthDate: '',
-    email: '',
-    password: '',
-    countryBirth:'',
-    phoneNumber:'',
-    gender:'',
-    bloodType:'',
-    photo:'',
+  formSubmit() {
+    if (this.customGender && this.customGender === 'Other') {
+      this.signupForm.patchValue({ gender: this.signupForm.get('gender')?.value });
+    } else if (this.customGender) {
+      this.signupForm.patchValue({ gender: this.customGender });
     }
+
+    if (this.signupForm.invalid) {
+      this.snack.open('Fill out the required fields', 'OK', {
+        duration: 3000,
+        verticalPosition: 'top',
+      });
+      return;
+    }
+
+    this.userService.addUser(this.signupForm.value).subscribe(
+      (data) => {
+        Swal.fire('User saved successfully', 'User saved in the system', 'success');
+      },
+      (error) => {
+        this.snack.open('System Error, Try later', 'OK', {
+          duration: 3000,
+          verticalPosition: 'top',
+        });
+      }
+    );
+  }
+
+  clear() {
+    this.signupForm.reset();
+    this.customGender = '';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { materialModule } from './app/material.imports';
+import { materialImports } from './app/material.imports';
 import { authInterceptorProviders } from './app/services/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
     ...appConfig.providers,
     provideHttpClient(withInterceptorsFromDi()),
-    materialModule,
+    materialImports(),
     authInterceptorProviders,
   ],
 })
